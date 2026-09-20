61215533fe1e98aa2d015e93d9578e736aeae3ff
Tatoh <tatukis@gmail.com>
2026-09-19 22:42:32 -0300

    feat(games): layout de paneles independiente del juego

---
+++ test/panel_layout_test.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:contador_de_truco/games/panel_layout.dart';
+
+void main() {
+  test('dos jugadores: apilados en vertical, lado a lado en horizontal', () {
+    expect(layoutFor(2, Orientation.portrait), [
+      [0],
+      [1],
+    ]);
+    expect(layoutFor(2, Orientation.landscape), [
+      [0, 1],
+    ]);
+  });
+
+  test('tres jugadores siguen la orientación', () {
+    expect(layoutFor(3, Orientation.portrait), [
+      [0],
+      [1],
+      [2],
+    ]);
+    expect(layoutFor(3, Orientation.landscape), [
+      [0, 1, 2],
+    ]);
+  });
+
+  test('cuatro jugadores: grilla 2x2 en cualquier orientación', () {
+    const grilla = [
+      [0, 1],
+      [2, 3],
+    ];
+    expect(layoutFor(4, Orientation.portrait), grilla);
+    expect(layoutFor(4, Orientation.landscape), grilla);
+  });
+
+  test('cinco y seis se parten en dos filas', () {
+    expect(layoutFor(5, Orientation.portrait), [
+      [0, 1, 2],
+      [3, 4],
+    ]);
+    expect(layoutFor(6, Orientation.landscape), [
+      [0, 1, 2],
+      [3, 4, 5],
+    ]);
+  });
+
+  test('todos los índices aparecen exactamente una vez', () {
+    for (final cantidad in [2, 3, 4, 5, 6]) {
+      for (final o in Orientation.values) {
+        final planos = layoutFor(cantidad, o).expand((f) => f).toList()..sort();
+        expect(planos, List.generate(cantidad, (i) => i),
+            reason: 'cantidad=$cantidad orientacion=$o');
+      }
+    }
+  });
+}
